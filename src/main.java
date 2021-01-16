553f4d2e425a98146ab278315218309087f3054c
Ayah N <ayah.noor0213@gmail.com>
2021-01-16 18:52:20 -0500

    i added new main file and some msges into new Project Parctice 2

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+        System.out.println("Hello Batch 21 from Ayah");
+
+        System.out.println("My name is Ayah I was previously studying in batch 15");
+
+
+    }
+}

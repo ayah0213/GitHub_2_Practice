7a53741493231ae466aff46178e205e77bd79a6e
Ayah N <ayah.noor0213@gmail.com>
2021-01-16 18:56:48 -0500

    2nd updated txt's

--- src/main.java
+++ src/main.java
@@ -8,6 +8,14 @@ public class main {
         System.out.println("Hello Batch 21 from Ayah");
 
         System.out.println("My name is Ayah I was previously studying in batch 15");
+        System.out.println("Tuncay");
+        System.out.println("Oktay");
+        System.out.println("Dovran");
+        System.out.println("Alison");
+        System.out.println();
+        System.out.println("Hello");
+        System.out.println("Cybertek");
+        System.out.println("Mclean");
 
 
     }
